e5827e4bbd49c7a0df04fa83ed47aa2ce5520c35
ncp204 <giodongnamayv3@gmail.com>
2023-04-21 15:24:29 +0700

    inital project

---
+++ DocterBooking/src/main/java/vn/edu/nlu/DocterBookingApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocterBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocterBookingApplication.class, args);
+	}
+
+}
---
+++ DocterBooking/src/test/java/vn/edu/nlu/DocterBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocterBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
